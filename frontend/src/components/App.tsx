21ef60acd5b6e3b3f622ec533cdffa3d98e3b776
obake-fe <daichismash@gmail.com>
2021-09-26 19:30:14 +0900

    feat: add material-ui pagination

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import '../styles/dist/tailwind.css';
+import "../styles/dist/tailwind.css";
 import { ReactPaginateCore } from "./React-Paginate";
-
-
+import { MuiPagination } from "./MuiPagination";
 
 function App() {
-
   return (
     <div className="w-1080 mx-auto">
       <h1 className="mt-4">React Pagination</h1>
       <ReactPaginateCore />
+      <MuiPagination />
     </div>
   );
 }
---
+++ frontend/src/components/MuiPagination.tsx
@@ -0,0 +1,59 @@
+import React, { useState, ChangeEvent } from "react";
+import { gql, useQuery } from "@apollo/client";
+import "../styles/dist/tailwind.css";
+import Pagination from "@material-ui/lab/Pagination";
+import { PageItems } from "./PageItems";
+
+const pokemonQuery = gql`
+  query GetPokemon {
+    pokemon {
+      id
+      name
+    }
+  }
+`;
+
+export const MuiPagination = () => {
+  // ページネーション設定
+  const [offset, setOffset] = useState(0); // 何番目のアイテムから表示するか
+  const [page, setPage] = useState(1); // ページ番号
+  const perPage = 5; // 1ページあたりに表示したいアイテムの数
+  // クリック時のfunction
+  const handlePageChange = (event: ChangeEvent<unknown>, index: number) => {
+    setPage(index);
+    const page_number = index - 1;
+    setOffset(page_number * perPage); // offsetを変更し、表示開始するアイテムの番号を変更
+  };
+
+  // クエリーデータ
+  const queryData = useQuery(pokemonQuery);
+  const { loading, error, data } = queryData;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <section className="mt-16">
+      <h2>
+        <a
+          href="https://mui.com/components/pagination/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ②Material-UI Pagination
+        </a>
+      </h2>
+      <PageItems {...{ data, offset, perPage }} />
+      {/* // ページネーションを置きたい箇所に以下のコンポーネントを配置*/}
+      <div className="w-full m-auto text-center">
+        <Pagination
+          count={10} // 総ページ数
+          color="primary" // ページネーションの色
+          onChange={handlePageChange} // 変更されたときに走る関数。第2引数にページ番号が入る
+          page={page} // 現在のページ番号
+          classes={{ root: "inline-block" }}
+        />
+      </div>
+    </section>
+  );
+};
