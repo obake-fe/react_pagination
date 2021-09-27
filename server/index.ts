14692c7f9d6fb5de4b39f029d30365239e992b59
obake-fe <daichismash@gmail.com>
2021-09-28 00:45:24 +0900

    feat: monorepo setting
    - eslint
    - prettier
    - husky

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,37 @@
-const fs = require('fs');
-const pokeJson = JSON.parse(fs.readFileSync("./pokedata.json", 'utf8'));
+const fs = require("fs");
 
-const { ApolloServer, gql } = require('apollo-server');
+const pokeJson = JSON.parse(fs.readFileSync("./pokedata.json", "utf8"));
+
+const { ApolloServer, gql } = require("apollo-server");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 // スキーマを定義する
 const typeDefs = gql`
-
   type Pokemon {
     id: ID
     name: String
   }
-  
+
   type Query {
     pokemon: [Pokemon]
   }
 `;
 
 // クエリで取得するデータ
-const pokeData = pokeJson.filter((json) => json.id < 252)
-.map((filteredData) => {
-  return ({
-    id: filteredData.id, name: filteredData.name.japanese
-  })
-})
-
+const pokeData = pokeJson
+  .filter((json) => json.id < 252)
+  .map((filteredData) => ({
+    id: filteredData.id,
+    name: filteredData.name.japanese
+  }));
 
 // クエリ発行時の処理を指定する
 const resolvers = {
   Query: {
-    pokemon: () => pokeData,
-  },
+    pokemon: () => pokeData
+  }
 };
 
 // The ApolloServer constructor requires two parameters: your schema
@@ -42,7 +41,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
   console.log(`🚀  Server ready at ${url}`);
 });
-
-
